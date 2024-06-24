b542f53aa4a97e4a70f95e8f5e56aa4dcfa672b5
AC GAMES <66536793+Anthonyzok521@users.noreply.github.com>
2024-06-24 12:33:19 -0400

    feat: Added Cards

--- client/src/components/Cards/Cards.tsx
+++ client/src/components/Cards/Cards.tsx
@@ -6,11 +6,28 @@ type Props = {
 };
 
 export const Cards: React.FC<Props> = (props: Props) => {
+
   return (
-    <div className="h-60 w-60 rounded-lg bg-white p-5">
+    <div className="h-56 w-56 rounded-lg bg-white flex flex-col justify-between overflow-hidden">
       <header>
-        <h1 className="text-xl text-black">{props.title}</h1>
+        <h1 className="text-xl text-center text-black">{props.title}</h1>
       </header>
+      <div className="w-full p-4 flex justify-center">
+        <a href={props.link} className="w-full hover:animate-bounce transition-all flex justify-center">
+        <img
+          src={props.image}
+          className="max-h-20 max-w-20 object-cover rounded-lg "
+          alt={props.title}
+        />
+        </a>
+      </div>
+      <div className="bg-amber-300 h-10 hover:h-20 hover:cursor-pointer transition-all">
+        <div className="flex flex-col justify-center items-center p-3 ">
+          <h3 className="text-black font-bebas uppercase">Descripción</h3>
+        <p className="text-center text-black">{props.description}</p>
+        </div>
+        
+      </div>
     </div>
   );
 };
--- client/src/components/UI/Main/Main.tsx
+++ client/src/components/UI/Main/Main.tsx
@@ -1,9 +1,54 @@
-import { Cards } from "../../Cards/Cards"
+import { Cards } from "../../Cards/Cards";
+import youtube from "/assets/youtube.svg";
+import spotify from "/assets/spotify.svg";
+import facebook from "/assets/facebook.svg";
+import telegram from "/assets/telegram.svg";
+
+interface Links{
+  youtube: string;
+  spotify: string;
+  facebook: string;
+  telegram: string;
+}
+
+const links:Links = {
+  youtube: import.meta.env.VITE_YOUTUBE,
+  spotify: import.meta.env.VITE_SPOTIFY,
+  facebook: import.meta.env.VITE_FACEBOOK,
+  telegram: import.meta.env.VITE_TELEGRAM
+}
+
+console.log(links)
 
 export const Main: React.FC = () => {
   return (
-    <main>
-      <Cards title="Cards" link="/cards" description="Cards" image="/assets/Cards.png"/>
+    <main className="w-full flex justify-center items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        <Cards
+          title="Youtube"
+          link={links.youtube}
+          description="Siguenos en nuestro canal de Youtube"
+          image={youtube}
+        />
+        <Cards
+          title="Telegram"
+          link={links.telegram}
+          description="Tenemos un canal de Telegram"
+          image={telegram}
+        />
+        <Cards
+          title="Spotify"
+          link={links.spotify}
+          description="Escuchanos en nuestro canal de Spotify"
+          image={spotify}
+        />
+        <Cards
+          title="Facebook"
+          link={links.facebook}
+          description="Únete a nuestra sociedad sigma en Facebook"
+          image={facebook}
+        />
+      </div>
     </main>
-  )
-}
+  );
+};
